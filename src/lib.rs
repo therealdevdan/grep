59f4de32b8289a5d578ed233e126ca3b39e89e92
Alex <alexanderdanilchenko888@gmail.com>
2025-06-08 02:01:28 +0300

    lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -69,34 +69,3 @@ pub fn search_case_insensitive<'a>(
     }
     results
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn case_sensitive() {
-        let query: &'static str = "duct";
-        let contents: &'static str = "\
-Rust:
-safe, fast, productive.
-Pick there,";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query: &'static str = "rUst";
-        let contents: &'static str = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
-    }
-} 
