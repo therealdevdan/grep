b9a9cd918577347a9f72c85aac627c36c2e9f9e3
Alex <alexanderdanilchenko888@gmail.com>
2025-07-23 07:17:56 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use std::env;
-use std::process;
+use std::{env, process};
 
 use minigrep::Config;
 
-fn main() {    
-    let config: Config = Config::build(env::args()).unwrap_or_else(|err: &'static str| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    println!("Serching for {}", config.query);
-    println!("In file {}", config.file_path);
-    if let Err(e)  = minigrep::run(config) {
-        eprintln!("Application error: {e}");
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let config: Config = match Config::build(&args) {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Problem parsing args: {err}");
+            process::exit(1);
+        }
+    };
+    
+    if let Err(e) = minigrep::run(config) { 
+        eprintln!("Application error: {e}"); 
         process::exit(1);
     }
 }
