b1a05f1999f743ad39c7f60270bf6a075ae3ac04
Alex <alexanderdanilchenko888@gmail.com>
2025-07-23 07:18:54 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -70,38 +70,3 @@ pub fn search_case_insensitive<'a>(
     }
     resuls
 }
-
-// # Tests
-#[cfg(test)]
-mod tests {
-    use std::vec;
-
-    use super::*;
-
-    #[test]
-    fn case_sensitive() {
-        let searching_word = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(searching_word, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let searching_word = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(searching_word, contents)
-        );
-    }
-}
